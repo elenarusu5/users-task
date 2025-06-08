4194a392a27f4017d2a122a5640581be34fce8db
elenarusu <rusuellenna@gmail.com>
2025-06-08 13:35:22 +0300

    cleanup; fetch users

---
+++ src/thunks/users.js
@@ -0,0 +1,10 @@
+
+export const fetchUsers = async () => {
+    try {
+        const response = await fetch('https://randomuser.me/api/?results=50')
+        const data = await response.json()
+        return data
+    } catch (error) {
+        return error
+    }
+}
