2ed409e4102dca4a8699a72d1d09ea98f1e6f10a
elenarusu <rusuellenna@gmail.com>
2025-06-09 09:49:26 +0300

    implemented "sort by" logic; tweaks for filtering, sorting, pagination

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
   const [itemsPerPage, setItemsPerPage] = useState(5)
   const [currentPage, setCurrentPage] = useState(1)
   const [inputSearch, setInputSearch] = useState("")
+  const [sort, setSort] = useState({ type: "", direction: "" })
 
   useEffect(() => {
     fetchUsers()
@@ -21,38 +22,90 @@ function App() {
       })
   }, [])
 
+  useEffect(() => {
+    if (!!sort.type && !!sort.direction) {
+      handleSort()
+    }
+  }, [sort.type, sort.direction])
+
   const handleModal = (user) => {
     setShowModal(true)
     setUserDetails(user)
   }
 
-  const startIndex = (currentPage - 1) * itemsPerPage
-  const endIndex = startIndex + itemsPerPage
-  const currentItems = users.slice(startIndex, endIndex);
+  const handleSort = () => {
+    let sortedUsers = [...users]
+    if (sort?.type === 'name') {
+      sortedUsers = [...users].sort((a, b) => {
+        const nameA = `${a.name.first} ${a.name.last}`.toLowerCase()
+        const nameB = `${b.name.first} ${b.name.last}`.toLowerCase()
+        return (
+          sort?.direction === 'asc' ? nameA.localeCompare(nameB)
+            : sort?.direction === 'desc' ? nameB.localeCompare(nameA)
+              : nameA.localeCompare(nameB)
+        )
+      })
+    } else if (sort?.type === 'email') {
+      sortedUsers = [...users].sort((a, b) => {
+        return (sort?.direction === 'asc' ? a.email.localeCompare(b.email)
+          : sort?.direction === 'desc' && b.email.localeCompare(a.email))
+      })
+    }
+    setUsers(sortedUsers)
+  }
 
-  const filteredItems = currentItems.filter((user) => {
+  const filteredUsers = [...users].filter((user) => {
+    const fullName = `${user.name.first} ${user.name.last}`.toLowerCase()
     return (
-      user.name.first.toLowerCase().includes(inputSearch.toLowerCase())
-      || user.name.last.toLowerCase().includes(inputSearch.toLowerCase())
+      fullName.includes(inputSearch.toLowerCase())
       || user.email.toLowerCase().includes(inputSearch.toLowerCase())
     )
   })
 
+  const items = inputSearch ? filteredUsers : users
+
+  const startIndex = (currentPage - 1) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+  const currentItems = items.slice(startIndex, endIndex)
+
   return (
     <div className="container mx-auto p-4">
-      <div className="flex justify-between items-center">
-        <p className="my-5 text-xl font-bold">Random Users:</p>
-        <div className="flex justify-between items-center mr-5 gap-5">
+      <div className="lg:flex lg:justify-between lg:items-center">
+        <p className="lg:my-5 text-xl font-bold">Random Users:</p>
+        <div className="lg:flex lg:justify-between lg:items-center lg:mr-5 lg:gap-5">
           <SearchInput
             inputSearch={inputSearch}
             setInputSearch={setInputSearch}
           />
+          <div className="flex gap-2">
+            <select
+              name="type"
+              className="bg-gray-200 text-gray-900 text-sm rounded-lg block w-40 p-3 mt-2"
+              value={sort.type}
+              onChange={(e) => setSort(prev => ({ ...prev, type: e.target.value }))}
+            >
+              <option value="" selected disabled>Sort by</option>
+              <option value="name">Sort by name</option>
+              <option value="email">Sort by email</option>
+            </select>
+
+            <select
+              name="direction"
+              className="bg-gray-200 text-gray-900 text-sm rounded-lg block w-40 p-3 mt-2"
+              value={sort.direction}
+              onChange={(e) => setSort(prev => ({ ...prev, direction: e.target.value }))}
+            >
+              <option value="" selected disabled>Direction</option>
+              <option value="asc">Sort by ASC</option>
+              <option value="desc">Sort by DESC</option>
+            </select>
+          </div>
         </div>
       </div>
 
       <div className="users-list overflow-y-auto">
-        {filteredItems?.length ?
-          filteredItems?.map((user) => (
+        {currentItems?.length ?
+          currentItems?.map((user) => (
             <UserCard
               user={user}
               handleModal={handleModal}
@@ -63,7 +116,8 @@ function App() {
           )}
       </div>
 
-      {!!showModal &&
+      {
+        !!showModal &&
         <Modal
           onClose={() => setShowModal(false)}
         >
@@ -75,20 +129,20 @@ function App() {
       }
 
       <Pagination
-        totalPages={Math.ceil(users.length / itemsPerPage)}
+        totalPages={Math.ceil(items.length / itemsPerPage)}
         currentPage={currentPage}
         itemsPerPage={itemsPerPage}
         onItemsPerPageChange={(value) => {
           setItemsPerPage(value)
-          if (currentPage > Math.ceil(users.length / value)) {
-            setCurrentPage(Math.ceil(users.length / value))
+          if (currentPage > Math.ceil(items.length / value)) {
+            setCurrentPage(Math.ceil(items.length / value))
           }
         }}
         onPageChange={(page) => {
           setCurrentPage(page)
         }}
       />
-    </div>
+    </div >
   )
 }
 
